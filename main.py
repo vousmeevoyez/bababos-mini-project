e19f92b7fa31b688a39532ebceb886d8026b3484
Kelvin <kelvin@Kelvins-MacBook-Air.local>
2024-02-01 21:28:10 +0700

    add sample

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ from solution import optimize_price
 
 sku_code = st.text_input('Enter sku code', 'SIK-123')
 
+with open("data/csv/Dataset 1 - historical PO data.csv", "rb") as file:
+    btn = st.download_button(
+            label="Download sample csv",
+            data=file,
+            file_name="bababos.csv",
+            mime="text/csv"
+          )
+
+
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
